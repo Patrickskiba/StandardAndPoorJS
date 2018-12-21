6b60148e38a8c6160d6de9b4b897b11e10ec245f
Patrick Skiba <pskiba@posteo.net>
2018-12-21 07:15:52 -0500

    returns a list of stocks in the s&p 500

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
-const request = require('request-promise-native')
+const https = require('https')
 const cheerio = require('cheerio')
 
+const url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+
 const FetchWikiPage = async () => {
-  const response = await request(
-    'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-  ).catch(console.error)
+  const response = await GetPageData()
   const $ = cheerio.load(response)
   return ParseHtmlTable($)
 }
+
+const GetPageData = () => new Promise((resolve, reject) => https.get(url, response => {
+  let data
+  response.on('data', chunk => (data += chunk))
+  response.on('end', () => resolve(data))
+  response.on('error', err => reject(err))
+}))
 
 const ParseHtmlTable = $ => {
   return $('.wikitable')
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,13 +1,13 @@
 const nock = require('nock')
 const { FetchWikiPage } = require('../src/main')
-const { mockedPageData } = require('./assertion-data/SP500WikiPage')
+const { mockedPageData } = require('./mock-data/SP500WikiPage')
 
 describe('main tests', async () => {
   it('should return a list of 500 stocks', async () => {
     nock('https://en.wikipedia.org').get('/wiki/List_of_S%26P_500_companies').reply(200, mockedPageData)
 
     const response = FetchWikiPage()
-    const expectedResult = require('./assertion-data/sp500')
+    const expectedResult = require('./assert-data/sp500')
 
     expect(await response).toEqual(expectedResult)
   })
