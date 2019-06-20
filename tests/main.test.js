aecba43d254b314ba180e47db9f14a7647ba487a
Patrick Skiba <pskiba@posteo.net>
2019-06-20 18:24:31 -0400

    Updating jest due to security concerns

--- tests/main.test.js
+++ tests/main.test.js
@@ -2,9 +2,11 @@ const nock = require('nock')
 const { FetchWikiPage } = require('../dist/main')
 const { mockedPageData } = require('./mock-data/SP500WikiPage')
 
-describe('main tests', async () => {
+describe('main tests', () => {
   it('should return a list of 500 stocks', async () => {
-    nock('https://en.wikipedia.org').get('/wiki/List_of_S%26P_500_companies').reply(200, mockedPageData)
+    nock('https://en.wikipedia.org')
+      .get('/wiki/List_of_S%26P_500_companies')
+      .reply(200, mockedPageData)
 
     const response = FetchWikiPage()
     const expectedResult = require('./assert-data/sp500')
